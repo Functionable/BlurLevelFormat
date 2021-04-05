e041c18785dbbe4ad9d12b8b8e9086f30dd140d4
Functionable <40835042+Functionable@users.noreply.github.com>
2021-04-05 23:35:53 +0100

    Changes that allow BLF to function better as a library
    Fixed the CMake file, and modified the blf include file to just include every blf header that is needed.

--- include/blf.hpp
+++ include/blf.hpp
@@ -4,3 +4,29 @@
  */
 
 #include "enums.hpp"
+#include "version.hpp"
+#include "blffile.hpp"
+#include "string.hpp"
+
+#include "templatearrayconstiterator.hpp"
+#include "templatecreator.hpp"
+#include "templateobject.hpp"
+
+#include "writer.hpp"
+#include "reader.hpp"
+
+#include "basereadable.hpp"
+#include "basewriteable.hpp"
+#include "dynamicreadable.hpp"
+#include "dynamicwriteable.hpp"
+
+#include "informationheader.hpp"
+#include "commontable.hpp"
+#include "objecttable.hpp"
+#include "datagroup.hpp"
+#include "datatable.hpp"
+
+#include "objectdefinition.hpp"
+#include "objectattribute.hpp"
+#include "definitioncreator.hpp"
+
